35f6467a491b6c895a30453aa6606e7b027ab726
IvanDrokin <ivan.s.drokin@bk.ru>
2015-05-10 01:10:53 +0300

    Performance improvement

--- gws/isomorph/graph_kernel.py
+++ gws/isomorph/graph_kernel.py
@@ -1,7 +1,7 @@
 from eden.graph import Vectorizer
 from sklearn import metrics
 from scipy.sparse import vstack
-import networkx as nx
+from internal import mol2nxgraph
 import numpy as np
 
 
@@ -39,37 +39,7 @@ def graph_kernel(mol_list, sm_list, mol_feat_list, mol_test, gk_param):
 def mol2graph(mol):
     adj1 = mol['g']
     atom1 = mol['atom']
-    b1 = np.zeros((np.shape(adj1)[0], len(atom1)))
-    c1 = 0
-    mp = np.zeros((len(atom1)), dtype='object')
-    mp[0] = atom1[0]
-    b1[0, 0] = 1
-    for i in range(1, len(atom1)):
-        ind = np.where(mp[0:(c1+1)] == atom1[i])[0]
-        if len(ind) > 0:
-            b1[i, ind] = 1
-        else:
-            c1 += 1
-            mp[c1] = atom1[i]
-            b1[i, c1] = 1
-    b1 = b1[:, 0:(c1+1)]
-    a1 = np.concatenate([atom1, mp])
-
-    tmp_zero = np.hstack((b1.transpose(), np.zeros((np.shape(b1)[1], np.shape(b1)[1]))))
-    tmp = np.hstack((adj1, b1))
-    a1b = np.vstack((tmp, tmp_zero))
-
-    g1 = nx.Graph()
-    n = np.shape(a1b)[0]
-    for i in range(n):
-        g1.add_node(i, label='')
-        for j in range(i+1, n):
-            if a1b[i, j] > 0:
-                g1.add_edge(i, j, weight=a1b[i, j], label='-')
-    for i in range(n):
-        g1.node[i]['lable'] = a1[i]
-
-    return g1
+    return mol2nxgraph(adj1, atom1)
 
 
 def get_def_par():

62b8709c07782b5cce1054bad5c31093ab58bd2e
IvanDrokin <ivan.s.drokin@bk.ru>
2015-05-10 02:46:23 +0300

    Improve gws.isomorph.internal.mol2nxgraph

--- gws/isomorph/internal.py
+++ gws/isomorph/internal.py
@@ -1,47 +1,33 @@
 import numpy as np
 import networkx as nx
-
+import networkx.algorithms.isomorphism as iso
 
 def graphs_isomorph_atom(adj1, adj2, atom1, atom2):
     nx_graph1 = mol2nxgraph(adj1, atom1)
     nx_graph2 = mol2nxgraph(adj2, atom2)
     is_iso = nx.faster_could_be_isomorphic(nx_graph1, nx_graph2)
+    nm = iso.categorical_node_match('label', 'C')
+    em = iso.categorical_edge_match(['weight', 'label'], [1, '-'])
     if is_iso:
-        return nx.is_isomorphic(nx_graph1, nx_graph2)
+        return iso.is_isomorphic(nx_graph1, nx_graph2, node_match=nm, edge_match=em)
     else:
         return is_iso
 
 
 def mol2nxgraph(adj, atom):
 
-    b1 = np.zeros((np.shape(adj)[0], len(atom)))
-    c1 = 0
-    mp = np.zeros((len(atom)), dtype='object')
-    mp[0] = atom[0]
-    b1[0, 0] = 1
-    for i in range(1, len(atom)):
-        ind = np.where(mp[0:(c1+1)] == atom[i])[0]
-        if len(ind) > 0:
-            b1[i, ind] = 1
-        else:
-            c1 += 1
-            mp[c1] = atom[i]
-            b1[i, c1] = 1
-    b1 = b1[:, 0:(c1+1)]
-    a1 = np.concatenate([atom, mp])
-
-    tmp_zero = np.hstack((b1.transpose(), np.zeros((np.shape(b1)[1], np.shape(b1)[1]))))
-    tmp = np.hstack((adj, b1))
-    a1b = np.vstack((tmp, tmp_zero))
-
     g1 = nx.Graph()
-    n = np.shape(a1b)[0]
+    n = np.shape(adj)[0]
     for i in range(n):
-        g1.add_node(i, label='')
+        g1.add_node(i, label=atom[i])
         for j in range(i+1, n):
-            if a1b[i, j] > 0:
-                g1.add_edge(i, j, weight=a1b[i, j], label='-')
-    for i in range(n):
-        g1.node[i]['label'] = a1[i]
+            if adj[i, j] > 0:
+                if adj[i, j] == 1:
+                    lb = '-'
+                elif adj[i, j] == 2:
+                    lb = '='
+                else:
+                    lb = '#'
+                g1.add_edge(i, j, weight=adj[i, j], label=lb)
 
     return g1

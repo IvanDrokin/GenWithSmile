78759471b77cac409ba8f0545f250f4a5d1dd894
Timofey Bondarev <bondarevts@gmail.com>
2015-05-12 15:44:58 +0000

    small improve graph_kernel.py

--- gws/isomorph/graph_kernel.py
+++ gws/isomorph/graph_kernel.py
@@ -15,37 +15,23 @@ def graph_kernel(mol_list, sm_list, mol_feat_list, mol_test, gk_param):
     nbits = gk_param['nbits']
     rjob = gk_param['rjob']
 
-    if len(mol_list) == 0:
-        sm_test = []
-        graphs_list = pre_process(mol_test)
-        vectorizer = Vectorizer(complexity=complexity, r=r, d=d, min_r=min_r, min_d=min_d, nbits=nbits)
+    vectorizer = Vectorizer(complexity=complexity, r=r, d=d, min_r=min_r, min_d=min_d, nbits=nbits)
+    graphs_list = (mol2nxgraph(mol['g'], mol['atom']) for mol in mol_test)
+
+    if not mol_list:
         mol_feat_list = vectorizer.transform(graphs_list, n_jobs=rjob)
-        for i in range(len(mol_test)):
-            sm_test += [mol_test[i]['smiles']]
+        sm_test = [mol['smiles'] for mol in mol_test]
         return mol_test, sm_test, mol_feat_list
 
-    graphs_test = pre_process(mol_test)
-    vectorizer = Vectorizer(complexity=complexity, r=r, d=d, min_r=min_r, min_d=min_d, nbits=nbits)
-    x_test = vectorizer.transform(graphs_test, n_jobs=rjob)
+    x_test = vectorizer.transform(graphs_list, n_jobs=rjob)
     k = metrics.pairwise.pairwise_kernels(mol_feat_list, x_test, metric='cosine')
-    for i in range(len(mol_test)):
+    for i, mol in enumerate(mol_test):
         if len(np.where(k[:, i] > p)[0]) < 1:
-            mol_list += [mol_test[i]]
-            sm_list += [mol_test[i]['smiles']]
+            mol_list.append(mol)
+            sm_list.append(mol['smiles'])
             mol_feat_list = vstack([mol_feat_list, x_test[i, ]])
     return mol_list, sm_list, mol_feat_list
 
 
-def mol2graph(mol):
-    adj1 = mol['g']
-    atom1 = mol['atom']
-    return mol2nxgraph(adj1, atom1)
-
-
 def get_def_par():
     return {'p': 0.99999, 'complexity': 4, 'r': None, 'd': None, 'min_r': 0, 'min_d': 0, 'nbits': 20, 'rjob': 1}
-
-
-def pre_process(iterable):
-    for seq in iterable:
-        yield mol2graph(seq)

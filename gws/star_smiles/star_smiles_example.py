608b8aa2b6aaa4d0aa9d81c5b1b0da275e907f49
IvanDrokin <ivan.s.drokin@bk.ru>
2015-05-14 17:33:43 +0300

    Fix import StarSmilesParser error

--- gws/star_smiles/star_smiles_example.py
+++ gws/star_smiles/star_smiles_example.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from parser_exceptions import StarSmilesFormatError
-from parser import StarSmilesParser
+from star_smiles_parser import StarSmilesParser
 
 
 def star_smiles_to_smiles(star_smiles):
